29ffb0aa393e4be475de47fb3422a9f4a6b95f5b
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2021-10-27 15:31:33 +0900

    files add

---
+++ C.C++CS-2021_10_27-HomeWork/C.C++CS-2021_10_27-HomeWork.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+/// <summary>
+/// オブジェクトクラス
+/// </summary>
+class Object
+{
+public:
+	Object()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	virtual void Update() {}
+private:
+	int x;
+	int y;
+	int z;
+};
+
+/// <summary>
+/// Vehicle(乗り物)クラス
+/// </summary>
+class Vehicle : Object
+{
+public:
+	Vehicle() {}
+private:
+	/// <summary>
+	/// 移動速度上昇関数
+	/// </summary>
+	class SpeedUp
+	{
+	public:
+		SpeedUp() {}
+	private:
+		int speed;
+		int axcel;
+	};
+};
+
+/// <summary>
+/// Car(車)クラス、Vehicle継承
+/// </summary>
+class Car : Vehicle
+{
+public:
+	Car() {}
+
+	void SetSpeed();
+private:
+
+};
+
+/// <summary>
+/// スピード設定用関数
+/// </summary>
+void Car::SetSpeed()
+{
+
+}
